42652e66bbd8edadda7236d7813a53209faa2f1a
VEERESH V PATIL <veereshvijay24082006@gmail.com>
2025-11-18 00:12:33 +0530

    Switched bcrypt to bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../database");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // 🔐 Helper: Create JWT token
