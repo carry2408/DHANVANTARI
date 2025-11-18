ab9a01e4d38725eaae13a45045a216d53c6e584d
VEERESH V PATIL <veereshvijay24082006@gmail.com>
2025-11-18 13:19:58 +0530

    updated port number for db init

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ init()
   .then(() => {
     console.log("Database initialized ✔");
 
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
+    app.listen(10000, () => {
+      console.log("Server running on port 10000");
     });
   })
   .catch(err => {
